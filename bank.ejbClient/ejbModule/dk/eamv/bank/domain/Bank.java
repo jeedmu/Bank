4a7aeee90799cfb8ef14715209b1f2401d53d3df
Jonaskjeld <Jonaskj@live.dk>
2019-02-26 11:10:06 +0100

    Made Bank and Property immutable

--- bank.ejbClient/ejbModule/dk/eamv/bank/domain/Bank.java
+++ bank.ejbClient/ejbModule/dk/eamv/bank/domain/Bank.java
@@ -2,14 +2,29 @@ package dk.eamv.bank.domain;
 
 public class Bank {
 
-	private String bankName;
+	private final String bankName;
+	
+	public static class Builder {
+		private final String bankName;
+		
+		public Builder(String bankName) {
+			this.bankName = bankName;
+		}
+		
+		public Bank Build() {
+			return new Bank(this);
+		}
+	}
+
+	private Bank(Builder builder) {
+		bankName = builder.bankName;
+	}
 
 	public String getBankName() {
 		return bankName;
 	}
 
-	public void setBankName(String bankName) {
-		this.bankName = bankName;
+	public Bank setBankName(String bankName) {
+		return new Bank.Builder(bankName).Build();
 	}
-	
 }
