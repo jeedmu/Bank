149469b8299c03d9c5b6ba20d331337e6af5bfb5
MikkelTheGreat <33924773+MikkelTheGreat@users.noreply.github.com>
2019-02-26 11:15:29 +0100

    Made Customer Immutable

--- bank.ejbClient/ejbModule/dk/eamv/bank/domain/Customer.java
+++ bank.ejbClient/ejbModule/dk/eamv/bank/domain/Customer.java
@@ -1,70 +1,176 @@
 package dk.eamv.bank.domain;
 
 public class Customer {
-	
-	private int customerID;
-	private String sSN;
-	private String firstName;
-	private String surName;
-	private String address;
-	private String country;
-	private String zipCode;
-	private String city;
-	private String email;
-	
+
+	private final int customerID;
+	private final String sSN;
+	private final String firstName;
+	private final String surName;
+	private final String address;
+	private final String country;
+	private final String zipCode;
+	private final String city;
+	private final String email;
+
+	public static class Builder {
+		// Required Params
+		private final int customerID;
+		private final String sSN;
+
+		// Optional Params
+		private String firstName;
+		private String surName;
+		private String address;
+		private String country;
+		private String zipCode;
+		private String city;
+		private String email;
+
+		public Builder(int customerID, String sSN) {
+			this.customerID = customerID;
+			this.sSN = sSN;
+		}
+
+		public Builder setFirstName(String firstName) {
+			this.firstName = firstName;
+			return this;
+		}
+
+		public Builder setSurName(String surName) {
+			this.surName = surName;
+			return this;
+		}
+
+		public Builder setAddress(String address) {
+			this.address = address;
+			return this;
+		}
+
+		public Builder setCountry(String country) {
+			this.country = country;
+			return this;
+		}
+
+		public Builder setZipCode(String zipCode) {
+			this.zipCode = zipCode;
+			return this;
+		}
+
+		public Builder setCity(String city) {
+			this.city = city;
+			return this;
+		}
+
+		public Builder setEmail(String email) {
+			this.email = email;
+			return this;
+		}
+
+		public Customer Build() {
+			return new Customer(this);
+		}
+	}
+
+	private Customer(Builder builder) {
+		customerID = builder.customerID;
+		sSN = builder.sSN;
+		firstName = builder.firstName;
+		surName = builder.surName;
+		address = builder.address;
+		country = builder.country;
+		zipCode = builder.zipCode;
+		city = builder.city;
+		email = builder.email;
+	}
+
 	public int getCustomerID() {
 		return customerID;
 	}
-	public void setCustomerID(int customerID) {
-		this.customerID = customerID;
+
+	public Customer setCustomerID(int customerID) {
+		return new Customer.Builder(customerID, this.sSN).setFirstName(this.firstName).setSurName(this.surName)
+				.setAddress(this.address).setCountry(this.country).setZipCode(this.zipCode).setCity(this.city)
+				.setEmail(this.email).Build();
 	}
+
 	public String getSSN() {
 		return sSN;
 	}
-	public void setSSN(String sSN) {
-		this.sSN = sSN;
+
+	public Customer setSSN(String sSN) {
+		return new Customer.Builder(this.customerID, sSN).setFirstName(this.firstName).setSurName(this.surName)
+				.setAddress(this.address).setCountry(this.country).setZipCode(this.zipCode).setCity(this.city)
+				.setEmail(this.email).Build();
 	}
+
 	public String getFirstName() {
 		return firstName;
 	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
+
+	public Customer setFirstName(String firstName) {
+		return new Customer.Builder(this.customerID, this.sSN).setFirstName(firstName).setSurName(this.surName)
+				.setAddress(this.address).setCountry(this.country).setZipCode(this.zipCode).setCity(this.city)
+				.setEmail(this.email).Build();
 	}
+
 	public String getSurName() {
 		return surName;
 	}
-	public void setSurName(String surName) {
-		this.surName = surName;
+
+	public Customer setSurName(String surName) {
+		return new Customer.Builder(this.customerID, this.sSN).setFirstName(this.firstName).setSurName(surName)
+				.setAddress(this.address).setCountry(this.country).setZipCode(this.zipCode).setCity(this.city)
+				.setEmail(this.email).Build();
 	}
+
 	public String getAddress() {
 		return address;
 	}
-	public void setAddress(String address) {
-		this.address = address;
+
+	public Customer setAddress(String address) {
+		return new Customer.Builder(this.customerID, this.sSN).setFirstName(this.firstName).setSurName(this.surName)
+				.setAddress(address).setCountry(this.country).setZipCode(this.zipCode).setCity(this.city)
+				.setEmail(this.email).Build();
 	}
+
 	public String getCountry() {
 		return country;
 	}
-	public void setCountry(String country) {
-		this.country = country;
+
+	public Customer setCountry(String country) {
+		return new Customer.Builder(this.customerID, this.sSN).setFirstName(this.firstName).setSurName(this.surName)
+				.setAddress(this.address).setCountry(country).setZipCode(this.zipCode).setCity(this.city)
+				.setEmail(this.email).Build();
 	}
+
 	public String getZipCode() {
 		return zipCode;
 	}
-	public void setZipCode(String zipCode) {
-		this.zipCode = zipCode;
+
+	public Customer setZipCode(String zipCode) {
+		return new Customer.Builder(this.customerID, this.sSN).setFirstName(this.firstName).setSurName(this.surName)
+				.setAddress(this.address).setCountry(this.country).setZipCode(zipCode).setCity(this.city)
+				.setEmail(this.email).Build();
 	}
+
 	public String getCity() {
 		return city;
 	}
-	public void setCity(String city) {
-		this.city = city;
+
+	public Customer setCity(String city) {
+		return new Customer.Builder(this.customerID, this.sSN).setFirstName(this.firstName).setSurName(this.surName)
+				.setAddress(this.address).setCountry(this.country).setZipCode(this.zipCode).setCity(city)
+				.setEmail(this.email).Build();
 	}
+
 	public String getEmail() {
 		return email;
 	}
-	public void setEmail(String email) {
-		this.email = email;
+
+	public Customer setEmail(String email) {
+		return new Customer.Builder(this.customerID, this.sSN).setFirstName(this.firstName).setSurName(this.surName)
+				.setAddress(this.address).setCountry(this.country).setZipCode(this.zipCode).setCity(this.city)
+				.setEmail(email).Build();
 	}
 
 }
