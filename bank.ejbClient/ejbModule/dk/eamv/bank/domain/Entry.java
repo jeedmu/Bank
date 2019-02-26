1c2aa1ad71b7ef60f95e1ebaa0511db1dd5ce435
Neller300 <neller_300@yahoo.dk>
2019-02-26 09:51:55 +0100

    Added last domain class
    Entry class added to domain

---
+++ bank.ejbClient/ejbModule/dk/eamv/bank/domain/Entry.java
@@ -0,0 +1,37 @@
+package dk.eamv.bank.domain;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Entry {
+	
+	private int EntryID;
+	private String Description;
+	private LocalDateTime Date;
+	private BigDecimal Amount;
+	
+	public int getEntryID() {
+		return EntryID;
+	}
+	public void setEntryID(int entryID) {
+		EntryID = entryID;
+	}
+	public String getDescription() {
+		return Description;
+	}
+	public void setDescription(String description) {
+		Description = description;
+	}
+	public LocalDateTime getDate() {
+		return Date;
+	}
+	public void setDate(LocalDateTime date) {
+		Date = date;
+	}
+	public BigDecimal getAmount() {
+		return Amount;
+	}
+	public void setAmount(BigDecimal amount) {
+		Amount = amount;
+	}
+}
