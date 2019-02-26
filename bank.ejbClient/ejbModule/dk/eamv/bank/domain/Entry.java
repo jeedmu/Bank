1453e41951b8e77344a1e301181329d0e27a7e5d
Neller300 <neller_300@yahoo.dk>
2019-02-26 11:11:15 +0100

    Made Entry class Immutable

--- bank.ejbClient/ejbModule/dk/eamv/bank/domain/Entry.java
+++ bank.ejbClient/ejbModule/dk/eamv/bank/domain/Entry.java
@@ -3,42 +3,83 @@ package dk.eamv.bank.domain;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-public class Entry {
+public final class Entry {
 	
-	private int entryID;
-	private String description;
-	private LocalDateTime date;
-	private BigDecimal amount;
-	private String accountNumber;
+	private final int entryID;
+	private final String description;
+	private final LocalDateTime date;
+	private final BigDecimal amount;
+	private final String accountNumber;
 	
+	public static class Builder{
+		private final int entryID;
+		private String description;
+		private final LocalDateTime date;
+		private final BigDecimal amount;
+		private final String accountNumber;
+		
+		public Builder(int entryID, LocalDateTime date, BigDecimal amount, String accountNumber) {
+			this.entryID = entryID;
+			this.date = date;
+			this.amount = amount;
+			this.accountNumber = accountNumber;
+		}
+		
+		public Builder setDescription(String description) {
+			this.description = description;
+			return this;
+		}
+		
+		public Entry Build() {
+			return new Entry(this);
+		}
+	}
+	
+	public Entry(Builder builder) {
+		entryID = builder.entryID;
+		description = builder.description;
+		date = builder.date;
+		amount = builder.amount;
+		accountNumber = builder.accountNumber;
+	}
 	public int getEntryID() {
 		return entryID;
 	}
-	public void setEntryID(int entryID) {
-		this.entryID = entryID;
+	public Entry setEntryID(int entryID) {
+		return new Entry.Builder(entryID, this.date, this.amount, this.accountNumber)
+				  .setDescription(this.description)
+				  .Build();
 	}
 	public String getDescription() {
 		return description;
 	}
-	public void setDescription(String description) {
-		this.description = description;
+	public Entry setDescription(String description) {
+		return new Entry.Builder(this.entryID, this.date, this.amount, this.accountNumber)
+				  .setDescription(description)
+				  .Build();
 	}
 	public LocalDateTime getDate() {
 		return date;
 	}
-	public void setDate(LocalDateTime date) {
-		this.date = date;
+	public Entry setDate(LocalDateTime date) {
+		return new Entry.Builder(this.entryID, date, this.amount, this.accountNumber)
+				  .setDescription(this.description)
+				  .Build();
 	}
 	public BigDecimal getAmount() {
 		return amount;
 	}
-	public void setAmount(BigDecimal amount) {
-		this.amount = amount;
+	public Entry setAmount(BigDecimal amount) {
+		return new Entry.Builder(this.entryID, this.date, amount, this.accountNumber)
+				  .setDescription(this.description)
+				  .Build();
 	}
 	public String getAccountNumber() {
 		return accountNumber;
 	}
-	public void setAccountNumber(String accountNumber) {
-		this.accountNumber = accountNumber;
+	public Entry setAccountNumber(String accountNumber) {
+		return new Entry.Builder(this.entryID, this.date, this.amount, accountNumber)
+				  .setDescription(this.description)
+				  .Build();
 	}
 }
