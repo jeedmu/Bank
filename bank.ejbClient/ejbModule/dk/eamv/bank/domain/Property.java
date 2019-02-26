2f770d2858f328c9d584fd9c5757926cdeb1b919
danieljensen96 <danielsj96@gmail.com>
2019-02-26 10:02:36 +0100

    Added 2 domain classes

---
+++ bank.ejbClient/ejbModule/dk/eamv/bank/domain/Property.java
@@ -0,0 +1,16 @@
+package dk.eamv.bank.domain;
+
+public class Property {
+
+	private String RegNumber;
+
+	public String getRegNumber() {
+		return RegNumber;
+	}
+
+	public void setRegNumber(String regNumber) {
+		RegNumber = regNumber;
+	}
+	
+	
+}
