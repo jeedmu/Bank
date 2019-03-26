262accb7f10b9dd00f3a648466f9d9202edb7f02
JTKlemmensen <25729544+JTKlemmensen@users.noreply.github.com>
2019-03-26 10:08:55 +0100

    Added customersearchparameters
    Avoid many parameters in the search method

---
+++ bank.ejbClient/ejbModule/dk/eamv/bank/domain/CustomerSearchParameters.java
@@ -0,0 +1,34 @@
+package dk.eamv.bank.domain;
+
+public class CustomerSearchParameters {
+
+	public String getSsn()
+	{
+		return "";
+	}
+
+	public String getFirstName()
+	{
+		return "";
+	}
+	
+	public String getLastName()
+	{
+		return "";
+	}
+	
+	public String getAddress()
+	{
+		return "";
+	}
+	
+	public String getEmail()
+	{
+		return "";
+	}
+	
+	public String getPhoneNumber()
+	{
+		return "";
+	}
+}
