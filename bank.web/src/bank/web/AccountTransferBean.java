acf530109bee13fb8915d60580a30b6d54a2935f
GoofyGlenn <33924992+GoofyGlenn@users.noreply.github.com>
2019-03-26 10:32:10 +0100

    added dropdown and calender to accountransfer class

--- bank.web/src/bank/web/AccountTransferBean.java
+++ bank.web/src/bank/web/AccountTransferBean.java
@@ -5,8 +5,7 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import javax.annotation.ManagedBean;
 import javax.ejb.EJB;
@@ -28,14 +27,29 @@ public class AccountTransferBean implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 
+	Account opsparing = new Account.Builder(25,9108,24242424).setBalance(BigDecimal.ZERO).setAccountName("Opsparing").build();
+	Account ferie = new Account.Builder(11,2201, 894891981).setBalance(BigDecimal.ONE).setAccountName("Ferie").build();
+	 private String account;  
+	 
+	 
+	 public String getAccount() {
+		 return account;
+	 }
 
-	public List<Account> getAccounts()
+	public Map<String, Account> getAccounts()
 	{
-		ArrayList<Account> ac = new ArrayList<Account>();
-		ac.add(new Account.Builder(25,9108,24242424).setBalance(BigDecimal.ZERO).setAccountName("Opsparing").build());
-		ac.add(new Account.Builder(11,2201, 894891981).setBalance(BigDecimal.ONE).setAccountName("Ferie").build());
-		ac.add(new Account.Builder(34,3487, 348584598).setBalance(BigDecimal.TEN.multiply(BigDecimal.TEN).multiply(BigDecimal.TEN).multiply(BigDecimal.TEN)).setAccountName("Kort").build());
-		return ac;
+//		ArrayList<Account> ac = new ArrayList<Account>();
+//		ac.add(new Account.Builder(25,9108,24242424).setBalance(BigDecimal.ZERO).setAccountName("Opsparing").build());
+//		ac.add(new Account.Builder(11,2201, 894891981).setBalance(BigDecimal.ONE).setAccountName("Ferie").build());
+//		ac.add(new Account.Builder(34,3487, 348584598).setBalance(BigDecimal.TEN.multiply(BigDecimal.TEN).multiply(BigDecimal.TEN).multiply(BigDecimal.TEN)).setAccountName("Kort").build());
+//		return ac;
+
+	    Map<String,Account> accounts;
+		   accounts  = new HashMap<String, Account>();
+	        accounts.put("opsparing",opsparing );
+	        accounts.put("ferie",ferie );
+	        
+	        return accounts;
 		//return homeBanking.showAccounts("");
 	}
 	
