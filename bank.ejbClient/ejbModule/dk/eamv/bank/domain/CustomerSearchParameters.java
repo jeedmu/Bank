b70b155881a9f70ffdd4d771218548aa3f02353b
JTKlemmensen <25729544+JTKlemmensen@users.noreply.github.com>
2019-03-26 11:28:57 +0100

    Updated customersearchparameters

--- bank.ejbClient/ejbModule/dk/eamv/bank/domain/CustomerSearchParameters.java
+++ bank.ejbClient/ejbModule/dk/eamv/bank/domain/CustomerSearchParameters.java
@@ -1,34 +1,82 @@
 package dk.eamv.bank.domain;
 
 public class CustomerSearchParameters {
+	
+	private String sSN;
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String country;
+	private String email;
+	private String phoneNumber;
 
-	public String getSsn()
-	{
-		return "";
+	public String getSSN() {
+		return sSN;
 	}
 
-	public String getFirstName()
-	{
-		return "";
+
+	public void setSSN(String sSN) {
+		this.sSN = sSN;
 	}
-	
-	public String getLastName()
-	{
-		return "";
+
+
+	public String getFirstName() {
+		return firstName;
 	}
-	
-	public String getAddress()
-	{
-		return "";
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
 	}
-	
-	public String getEmail()
-	{
-		return "";
+
+
+	public String getLastName() {
+		return lastName;
 	}
-	
-	public String getPhoneNumber()
-	{
-		return "";
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
 	}
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
+	public String getCountry() {
+		return country;
+	}
+
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
 }
