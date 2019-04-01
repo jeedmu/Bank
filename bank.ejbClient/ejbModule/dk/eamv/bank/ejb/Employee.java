12872bcf5c6edb436492ccedd284cfc0303257a1
JTKlemmensen <25729544+JTKlemmensen@users.noreply.github.com>
2019-04-01 11:14:53 +0200

    removed old documentation

--- bank.ejbClient/ejbModule/dk/eamv/bank/ejb/Employee.java
+++ bank.ejbClient/ejbModule/dk/eamv/bank/ejb/Employee.java
@@ -25,11 +25,6 @@ public interface Employee {
 	 */
 	public List<Customer> getCustomers(CustomerSearchParameters parameters);
 	
-	/** 
-	 * Used for both updating and creating customer
-	 * @param customerData HashMap keys: "sSN", "firstName", "surName", "address", "country", "zipCode", "city", "email", "phoneNumber"
-	 * @return true if succeeded, else false
-	 */
 	public boolean editCustomer(Customer customerData, LocalDate changeDate);
 
 	public List<Account> getAccounts(int customerID);
@@ -38,11 +33,6 @@ public interface Employee {
 	
 	public boolean deleteCustomer(int customerID);
 
-	/**
-	 * Used for both updating and creating account
-	 * @param accountData HashMap keys: "customerID", "regNumber", "accountNumber", "accountName", "balance" 
-	 * @return true if succeeded, else false
-	 */
 	public boolean editAccount(Account account);
 
 	public boolean deleteAccount(int regNumber, int accountNo);	
