505d4faea605e4746ac4a80f7f0de45851045f61
kebabjohn <32761420+kebabjohn@users.noreply.github.com>
2019-04-02 11:21:19 +0200

    Admin interface

--- bank.ejb/ejbModule/dk/eamv/bank/ejb/AdminBean.java
+++ bank.ejb/ejbModule/dk/eamv/bank/ejb/AdminBean.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 
 import javax.ejb.Stateless;
 
+import dk.eamv.bank.domain.Property;
+import dk.eamv.bank.domain.Role;
 import dk.eamv.bank.domain.User;
 
 /**
@@ -27,6 +29,84 @@ public class AdminBean implements Admin {
 
 	@Override
 	public boolean deleteUser(String id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean createUser(User user) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean getUsers() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean getUser(String userId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean editRole(Role role) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteRole(int roleID) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean createRole(Role role) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean getRoles() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean getRole(int roleID) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean editProperty(Property property) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteProperty(String property) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean createProperty(Property property) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean getProperties() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean getProperty(String property) {
 		// TODO Auto-generated method stub
 		return false;
 	}
--- bank.ejbClient/ejbModule/dk/eamv/bank/ejb/Admin.java
+++ bank.ejbClient/ejbModule/dk/eamv/bank/ejb/Admin.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 
 import javax.ejb.Local;
 
+import dk.eamv.bank.domain.Property;
+import dk.eamv.bank.domain.Role;
 import dk.eamv.bank.domain.User;
 
 @Local
@@ -14,6 +16,31 @@ public interface Admin {
 	public boolean editUser(User user);
 
 	public boolean deleteUser(String userId);
-
+	
+	public boolean createUser(User user);
+	
+	public boolean getUsers();
+	
+	public boolean getUser(String userId);
+	
+	public boolean editRole(Role role);
+	
+	public boolean deleteRole(int roleID);
+	
+	public boolean createRole(Role role);
+	
+	public boolean getRoles();
+	
+	public boolean getRole(int roleID);
+	
+	public boolean editProperty(Property property);
+	
+	public boolean deleteProperty(String property);
+	
+	public boolean createProperty(Property property);
+	
+	public boolean getProperties();
+	
+	public boolean getProperty(String property);
 	
 }
