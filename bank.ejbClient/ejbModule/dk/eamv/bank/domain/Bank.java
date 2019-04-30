c8b10982cb015ba2140a5d4300b511790f9d12fe
ulla2814 <ularsen5@hotmail.com>
2019-04-30 10:21:25 +0200

    Update Bank.java
    Setters fjernet i Bank

--- bank.ejbClient/ejbModule/dk/eamv/bank/domain/Bank.java
+++ bank.ejbClient/ejbModule/dk/eamv/bank/domain/Bank.java
@@ -80,80 +80,33 @@ public class Bank {
 	public String getAddress() {
 		return address;
 	}
-	public Bank setAddress(String address) {
-		return new Bank.Builder().setCvr(this.cvr).setBankName(this.bankName)
-				.setRegNumber(this.regNumber).setZipCode(this.zipCode).setCity(this.city).setAddress(address)
-				.setAccountNumber(this.accountNumber).setPhoneNumber(this.phoneNumber).build();
-	}
 	
 	public String getCVR() {
 		return cvr;
 	}
 	
-	public Bank setCVR(String cvr) {
-		return new Bank.Builder().setCvr(this.cvr).setBankName(this.bankName)
-				.setRegNumber(this.regNumber).setZipCode(this.zipCode).setCity(this.city).setAddress(address)
-				.setAccountNumber(this.accountNumber).setPhoneNumber(this.phoneNumber).build();
-	}
-
 	public String getBankName() {
 		return bankName;
-	}
-
-	public Bank setBankName(String bankName) {
-		return new Bank.Builder().setCvr(this.cvr).setBankName(this.bankName)
-				.setRegNumber(this.regNumber).setZipCode(this.zipCode).setCity(this.city).setAddress(address)
-				.setAccountNumber(this.accountNumber).setPhoneNumber(this.phoneNumber).build();
 	}
 	
 	public int getRegNumber() {
 		return regNumber;
 	}
-
-	public Bank setRegNumber(int regNumber) {
-		return new Bank.Builder().setCvr(this.cvr).setBankName(this.bankName)
-				.setRegNumber(this.regNumber).setZipCode(this.zipCode).setCity(this.city).setAddress(address)
-				.setAccountNumber(this.accountNumber).setPhoneNumber(this.phoneNumber).build();
-	}
 	
 	public int getAccountNumber() {
 		return accountNumber;
-	}
-	
-	public Bank setAccountNumber(int accountNumber) {
-		return new Bank.Builder().setCvr(this.cvr).setBankName(this.bankName)
-				.setRegNumber(this.regNumber).setZipCode(this.zipCode).setCity(this.city).setAddress(address)
-				.setAccountNumber(this.accountNumber).setPhoneNumber(this.phoneNumber).build();
-	}
+	}	
 	
 	public String getPhoneNumber() {
 		return phoneNumber;
-	}
-	
-	public Bank setPhoneNumber(String phoneNumber) {
-		return new Bank.Builder().setCvr(this.cvr).setBankName(this.bankName)
-				.setRegNumber(this.regNumber).setZipCode(this.zipCode).setCity(this.city).setAddress(address)
-				.setAccountNumber(this.accountNumber).setPhoneNumber(this.phoneNumber).build();
-	}
+	}	
 	
 	public String getZipCode() {
 		return zipCode;
-	}
-	
-	public Bank setZipCode(String zipCode) {
-		return new Bank.Builder().setCvr(this.cvr).setBankName(this.bankName)
-				.setRegNumber(this.regNumber).setZipCode(this.zipCode).setCity(this.city).setAddress(address)
-				.setAccountNumber(this.accountNumber).setPhoneNumber(this.phoneNumber).build();
-	}
+	}	
 	
 	public String getCity() {
 		return city;
-	}
-	
-	public Bank setCity(String city) {
-		return new Bank.Builder().setCvr(this.cvr).setBankName(this.bankName)
-				.setRegNumber(this.regNumber).setZipCode(this.zipCode).setCity(this.city).setAddress(address)
-				.setAccountNumber(this.accountNumber).setPhoneNumber(this.phoneNumber).build();
-	}
+	}	
 	
 }
