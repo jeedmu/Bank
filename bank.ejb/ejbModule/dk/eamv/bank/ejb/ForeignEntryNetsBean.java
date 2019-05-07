516f63dd1a220dba752e9e204c5b4e56eecd1068
Mathias A. Lenz <32672754+MathiasALenz@users.noreply.github.com>
2019-05-07 10:36:58 +0200

    Updated ForeignEntryNetsBean
    Euruka :100:

--- bank.ejb/ejbModule/dk/eamv/bank/ejb/ForeignEntryNetsBean.java
+++ bank.ejb/ejbModule/dk/eamv/bank/ejb/ForeignEntryNetsBean.java
@@ -1,54 +1,51 @@
 package dk.eamv.bank.ejb;
 
-import java.util.Optional;
-
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 
-import dk.eamv.bank.domain.Account;
-import dk.eamv.bank.domain.Bank;
 import dk.eamv.bank.domain.Entry;
 import dk.eamv.bank.domain.Transfer;
+import dk.eamv.bank.ejb.entitybeans.AccountBean;
 import dk.eamv.bank.ejb.entitybeans.BankBean;
+import dk.eamv.bank.ejb.entitybeans.CustomerBean;
 
 @WebService
 @Stateless
 public class ForeignEntryNetsBean 
 {
 	@EJB BankBean bbejb;
-	@EJB HomeBankingBean hbbejb;
+	@EJB AccountBean abejb;
+	@EJB CustomerBean cbejb;
 	
 	@WebMethod
-	public boolean CreateNetsRequest(int fromReg,Entry entry, Account fromAccount, String description) 
+	public boolean CreateNetsRequest(int fromReg,Entry entry) 
 	{
 		try {
 			/*if(bbejb.read(fromReg).isPresent()) {
 				bbejb.read(fromReg).get().getAccountNumber();
 			}*/
-			int accountNumber = bbejb.read(fromReg).get().getAccountNumber();
+			
 			Transfer transfer = new Transfer();
-			//transfer.setAmount();
-			transfer.setFromAccount(fromAccount);
-			transfer.setToAccountAccountNumber(accountNumber);			
-			transfer.setToDescription(description);			
+			int accountNumber = bbejb.read(fromReg).get().getAccountNumber();
+			int customerID = abejb.read(accountNumber).get().getCustomerID();
+			
+			transfer.setAmount(entry.getAmount());
+			transfer.setFromAccount(abejb.read(accountNumber).get());
+			transfer.setToAccountAccountNumber(accountNumber);
+			transfer.setFromDescription("Overførsel fra " + fromReg);			
+			transfer.setToDescription(entry.getDescription());
 			transfer.setRegNumber(entry.getRegNumber());
-			//transfer.setCurrentCustomer(currentCustomer);
-			//transfer.setDate(date);
-
-			//hbbejb.transferEntry(tf);
+			transfer.setCurrentCustomer(cbejb.read(customerID).get());
+			transfer.setDate(entry.getDate());
 			
 			return true;
+			
 		} catch (Exception e) {
 			e.printStackTrace();
+			
 			return false;
 		}
-		// Vi mangler oplysninger om afsendende bank.
-		// Den skal bruges til at finde det kontonummer, som pengene skal hæves fra!
-		// Derefter skal der laves et Transfer-objekt, som anvendes i kald til 
-		// metoden transferEntry i HomeBankingBean
-		// Skal afklares med Alex, Jacob og Jonas
 	}
-
 }
