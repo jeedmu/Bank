c5f68b9369ae6dbd4c5b2ad7a1cb863e9a9b4a04
Neller300 <neller_300@yahoo.dk>
2019-05-07 11:17:37 +0200

    Initial setup requestHandler AdminGUI

--- bank.adminGui/src/controllers/PropertyController.java
+++ bank.adminGui/src/controllers/PropertyController.java
@@ -1,5 +1,32 @@
 package controllers;
 
-public class PropertyController {
+import java.util.List;
+import java.util.Optional;
 
+import data.RequestHandler;
+import dk.eamv.bank.domain.Property;
+
+public class PropertyController {
+	RequestHandler handler = new RequestHandler();
+
+	public Optional<Property> getProperty(String search) {
+		return handler.getProperty(search);
+	}
+
+	public List<Property> getProperties() {
+		return handler.getProperties();
+	}
+	
+	public void createBank(Property property) {
+		handler.createProperty(property);
+	}
+	
+	public void editBank(Property property) {
+		handler.editProperty(property);
+	}
+	
+	public void deleteProperty(String property) {
+		handler.deleteProperty(property);
+	}
+	
 }
--- bank.adminGui/src/controllers/RoleController.java
+++ bank.adminGui/src/controllers/RoleController.java
@@ -1,5 +1,33 @@
 package controllers;
 
+import java.util.List;
+import java.util.Optional;
+
+import data.RequestHandler;
+import dk.eamv.bank.domain.Role;
+
 public class RoleController {
 
+	RequestHandler handler = new RequestHandler();
+
+	public Optional<Role> getRole(int roleId) {
+		return handler.getRole(roleId);
+	}
+
+	public List<Role> getRoles() {
+		return handler.getRoles();
+	}
+	
+	public void createRole(Role role) {
+		handler.createRole(role);
+	}
+	
+	public void editRole(Role role) {
+		handler.editRole(role);
+	}
+	
+	public void deleteRole(int roleId) {
+		handler.deleteRole(roleId);
+	}
+	
 }
