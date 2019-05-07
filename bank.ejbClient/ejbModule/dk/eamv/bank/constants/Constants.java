7bb0c1cd22c8324f2060f839325a46ea5ea1e2a3
Sofie <blom.sofie1203@gmail.com>
2019-05-07 11:34:45 +0200

    changes to DataRequestHandler

--- bank.ejbClient/ejbModule/dk/eamv/bank/constants/Constants.java
+++ bank.ejbClient/ejbModule/dk/eamv/bank/constants/Constants.java
@@ -4,5 +4,6 @@ public class Constants {
 	public final static String adminRole="Admin";
 	public final static String employeeRole="Employee";
 	public final static String customerRole="customer";
+	public final static String serverUrl="http://LENNI:8080";
 
 }
