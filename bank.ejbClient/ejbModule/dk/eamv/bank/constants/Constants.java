e3fe501ce88ca4728699cddb0b597614717db764
JTKlemmensen <25729544+JTKlemmensen@users.noreply.github.com>
2019-05-13 11:29:01 +0200

    fixed role constants

--- bank.ejbClient/ejbModule/dk/eamv/bank/constants/Constants.java
+++ bank.ejbClient/ejbModule/dk/eamv/bank/constants/Constants.java
@@ -1,8 +1,8 @@
 package dk.eamv.bank.constants;
 
 public class Constants {
-	public final static String adminRole="Admin";
-	public final static String employeeRole="Employee";
+	public final static String adminRole="admin";
+	public final static String employeeRole="employee";
 	public final static String customerRole="customer";
 	public final static String serverUrl="http://LENNI:8080";
 
