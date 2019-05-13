a684cbf4b6678f10af92f274845454e4211b1feb
Sofie <blom.sofie1203@gmail.com>
2019-05-13 09:33:41 +0200

    update DataRequestHandler

--- bank.javafx/src/dk/eamv/bank/javafx/Implementations/DataRequestHandler.java
+++ bank.javafx/src/dk/eamv/bank/javafx/Implementations/DataRequestHandler.java
@@ -8,6 +8,7 @@ import org.apache.http.HttpHeaders;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 
@@ -21,46 +22,50 @@ import dk.eamv.bank.javafx.domain.Transfer;
 import dk.eamv.bank.javafx.interfaces.IDataRequestHandler;
 import dk.eamv.bank.constants.Constants;
 
-
 public class DataRequestHandler implements IDataRequestHandler {
 
 	private ObjectMapper mapper = new ObjectMapper();
 	private HttpClient client = HttpClientBuilder.create().build();
-	
-	@Override
-	public void createNewCustomer(Customer customer) {
-		// TODO Auto-generated method stub
-		
-	}
 
 	@Override
 	public void createNewAccount(Account account) {
 		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void createNewCustomer(Customer customer) {
+		String url = Constants.serverUrl + "/bank.ws/rest/employee/createCustomer";
 		
+		HttpPost post = new HttpPost();
+		post.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
+		
+
 	}
 
 	@Override
 	public List<Customer> getCustomers(String customerID, String name, String phoneNumber) {
-		
+
 		String url = Constants.serverUrl + "/bank.ws/rest/employee/customerSearch/";
 		List<Customer> result = null;
-		
+
 		HttpGet get = new HttpGet(url);
-		get.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");		
+		get.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
 		get.addHeader("phoneNumber", phoneNumber);
-		
+
 		try {
-			
+
 			HttpResponse response = client.execute(get);
 			HttpEntity entity = response.getEntity();
 			String string = EntityUtils.toString(entity);
 			System.out.println(string);
-			result = mapper.readValue(string, new TypeReference<List<Customer>>(){});
-			
+			result = mapper.readValue(string, new TypeReference<List<Customer>>() {
+			});
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		return result;
 	}
 
@@ -85,7 +90,7 @@ public class DataRequestHandler implements IDataRequestHandler {
 	@Override
 	public void createNewTransfer(Transfer transfer) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
